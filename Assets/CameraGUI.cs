2e7a7b9d70ace53bdf006d5a488ca711f5957cd2
jepp2078 <jepp2078@gmail.com>
2014-10-12 23:38:17 +0200

    Here we go!
    Added project base

---
+++ Assets/CameraGUI.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraGUI : MonoBehaviour {
+	public Texture2D CrossHairTex;
+	public GameObject[] objectArray;
+	public GameObject[] WeaponArray;
+
+	private GameObject CurrentWeapon;
+	private GameObject WeaponPosition;
+
+	private Rect CrossHairPosition;
+	private GameObject currentObject;
+	//private GameObject CameraObject;
+	private bool openMenu = false;
+	
+	private MouseLook CameraMouseLook;
+	private MouseLook FPCMouseLook;
+	
+	private float tempSensX;
+	private float tempSensY;
+	private bool camFrozen = false;
+	
+	void Start(){
+		GameObject CameraObject = GameObject.Find("Main Camera");
+		CameraMouseLook = CameraObject.GetComponent<MouseLook>();
+		
+		GameObject FPCObject = GameObject.Find("First Person Controller");
+		FPCMouseLook = FPCObject.GetComponent<MouseLook>();
+
+		tempSensX = FPCMouseLook.sensitivityX;
+		tempSensY = CameraMouseLook.sensitivityY;
+
+
+		WeaponPosition = GameObject.Find("WeaponPosition");
+		if(WeaponPosition==null)
+			Debug.LogError("WeaponPosition not found!");
+		//CurrentWeapon = WeaponArray[0];
+		switchWeapon(WeaponArray[0]);
+	}
+
+	void switchWeapon(GameObject Weapon){
+		Destroy(CurrentWeapon);
+
+		CurrentWeapon = Instantiate(Weapon, WeaponPosition.transform.position, WeaponPosition.transform.rotation) as GameObject;
+		CurrentWeapon.transform.parent = WeaponPosition.transform;
+		//CurrentWeapon.transform.position = WeaponPosition.transform.position;
+	}
+
+	void Update () {
+		CrossHairPosition = new Rect((Screen.width - CrossHairTex.width)/2, (Screen.height - CrossHairTex.height)/2, CrossHairTex.width, CrossHairTex.height);
+
+		if(Input.GetKey(KeyCode.Alpha1))
+			switchWeapon(WeaponArray[0]);
+		else if(Input.GetKey(KeyCode.Alpha2))
+			switchWeapon(WeaponArray[1]);
+
+
+		if(Input.GetKey(KeyCode.Q)){
+			if(!camFrozen){
+				tempSensX = FPCMouseLook.sensitivityX;
+				tempSensY = CameraMouseLook.sensitivityY;
+				
+				FPCMouseLook.sensitivityX = 0;
+				CameraMouseLook.sensitivityY = 0;
+				camFrozen = true;
+			}
+		}else{
+			if(camFrozen){
+				FPCMouseLook.sensitivityX = tempSensX;
+				CameraMouseLook.sensitivityY = tempSensY;
+				camFrozen = false;
+			}
+		}
+		
+		if(Input.GetKeyDown(KeyCode.Q)){
+			openMenu = true;
+		}
+		if(Input.GetKeyUp(KeyCode.Q)){
+			openMenu = false;
+		}
+	}
+	
+	void rezItem(GameObject rezObject){
+		RaycastHit hit;
+		Ray ray = new Ray (transform.position, transform.forward);
+		//Debug.DrawRay (transform.position, transform.forward*rayLenght, Color.green, 10, true);
+		
+		if (Physics.Raycast(ray, out hit, 100)){
+			Debug.DrawLine(ray.origin, hit.point, Color.green, 10, true);
+			
+			if (hit.collider){
+				Instantiate(rezObject, hit.point, Quaternion.identity);
+			}
+		}
+	}
+	
+	void OnGUI () {
+		GUI.DrawTexture(CrossHairPosition, CrossHairTex); //Draws the crosshair
+	
+		if(openMenu){
+			int boxL = 120; //Horizontal lenght of the menu
+			
+			// Make a background box
+			GUI.Box(new Rect(10,10,120,30+30*objectArray.Length), "Spawn Menu");
+			
+			for (int i=0; i < objectArray.Length; i++) {
+				if (GUI.Button (new Rect (20, 40+(30*i), boxL-20, 20), ""+objectArray[i].name)) {
+					rezItem(objectArray[i]);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/PhysicsGun.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsGun : MonoBehaviour {
+	Rigidbody currentObject;
+	RaycastHit hit;
+	Vector3 currentObjectOriginalPos;
+	GameObject GunPoint;
+	GameObject CameraView;
+
+	bool holdingObject;
+
+	public float rayLenght;
+	public float rayForce;
+	public float objectScale;
+
+	// Use this for initialization
+	void Start () {
+		holdingObject = false;
+		rayLenght = 100;
+		rayForce = 100;
+		objectScale = 1;
+		GunPoint = GameObject.Find("GunPoint");
+		CameraView = GameObject.Find("Main Camera");
+	}
+
+	// Update is called once per frame
+	void Update() {
+		bool fireDown = Input.GetButtonDown("Fire1");
+		if(Input.GetButton("Fire1")){
+			//Debug.DrawRay (CameraView.transform.position, CameraView.transform.forward*rayLenght, Color.green, 0.5f, true);
+		}
+		//if(fireDown && !holdingObject){
+		if(Input.GetButton("Fire1") && !holdingObject){
+			Ray ray = new Ray (CameraView.transform.position, CameraView.transform.forward);
+			//Debug.DrawRay (transform.position, transform.forward*rayLenght, Color.green, 10, true);
+
+			if (Physics.Raycast(ray, out hit, rayLenght)){
+				if (hit.rigidbody){
+					currentObject = hit.rigidbody;
+					holdingObject = true;
+					currentObjectOriginalPos = currentObject.position;
+					rayLenght = hit.distance;
+					Debug.Log ("Ray Lenght: " + rayLenght);
+					
+					//Debug.DrawRay (hit.point, hit.normal, Color.red, rayLenght, true);
+					//Debug.Log("hit: "+ hit.rigidbody.name);
+				}
+			}
+		}else if(Input.GetButtonUp("Fire1")){
+			holdingObject = false;
+			rayLenght = 100;
+			Debug.Log("releasing object");
+		}
+
+		if(holdingObject){
+			rayLenght += Input.GetAxis("Mouse ScrollWheel");
+		
+			Vector3 gunVector = CameraView.transform.TransformPoint(Vector3.forward*objectScale*rayLenght);
+			Vector3 objectToGun = gunVector-currentObject.position;
+
+			Debug.DrawLine (GunPoint.transform.position, gunVector, Color.blue, 1, true);
+			Debug.DrawLine(currentObject.position, gunVector, Color.yellow, 1, true);
+
+			Vector3 relativeHit = (hit.point-currentObjectOriginalPos)+currentObject.position;
+			objectToGun.Scale(new Vector3(rayForce, rayForce, rayForce));
+			
+			Vector3 CurrentObjectVelocity = currentObject.velocity;
+			CurrentObjectVelocity.Scale(new Vector3(10, 10, 10));
+			objectToGun += -CurrentObjectVelocity;
+			
+			currentObject.AddForce(objectToGun, ForceMode.Acceleration);
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
