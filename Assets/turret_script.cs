86e010549756b3714e0cb9183cca85def1f3a2b9
bubbles100 <bubbles100j@gmail.com>
2014-10-28 14:13:41 +0100

    added a turret

---
+++ Assets/turret_script.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turret_script : MonoBehaviour {
+	public GameObject turret_Body, turret_Gunmount, turret_Gun1, turret_Gun2;
+
+	private Quaternion startYaw, startPitch; 
+
+	// Use this for initialization
+	void Start () {
+		startYaw = turret_Body.transform.rotation;
+		startPitch = turret_Gunmount.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject Enemy = FindClosestEnemy();
+		if(Enemy != null){
+			turret_Body.transform.Rotate(0, 1, 0);
+		}
+	}
+
+	GameObject FindClosestEnemy() {
+		GameObject[] gos;
+		gos = GameObject.FindGameObjectsWithTag("Enemy");
+		GameObject closest = null;
+		float distance = Mathf.Infinity;
+		Vector3 position = transform.position;
+		foreach (GameObject go in gos) {
+			Vector3 diff = go.transform.position - position;
+			float curDistance = diff.sqrMagnitude;
+			if (curDistance < distance) {
+				closest = go;
+				distance = curDistance;
+			}
+		}
+		return closest;
+	}
+}
