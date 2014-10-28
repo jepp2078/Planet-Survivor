b847a906b4aba8c8555773fbf18f0209222ec216
bubbles100 <bubbles100j@gmail.com>
2014-10-28 15:42:54 +0100

    worked on turret

--- Assets/turret_script.cs
+++ Assets/turret_script.cs
@@ -2,21 +2,30 @@
 using System.Collections;
 
 public class turret_script : MonoBehaviour {
-	public GameObject turret_Body, turret_Gunmount, turret_Gun1, turret_Gun2;
+	public GameObject turret_Body, turret_Gunmount, turret_Gun1, turret_Gun2, turret_GunPoint;
 
-	private Quaternion startYaw, startPitch; 
+	private Quaternion startYaw, startPitch, start_turret_GunPoint; 
 
 	// Use this for initialization
 	void Start () {
 		startYaw = turret_Body.transform.rotation;
 		startPitch = turret_Gunmount.transform.rotation;
+		start_turret_GunPoint = turret_GunPoint.transform.rotation;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		GameObject Enemy = FindClosestEnemy();
 		if(Enemy != null){
-			turret_Body.transform.Rotate(0, 1, 0);
+			Debug.DrawRay(turret_GunPoint.transform.position, turret_GunPoint.transform.forward, Color.red, 0.1f);
+			Vector3 targetVector = Enemy.transform.position - turret_GunPoint.transform.position;
+			Vector3 deltaRotation = turret_GunPoint.transform.forward - targetVector.normalized;
+			turret_Body.transform.Rotate(0, deltaRotation.x, 0);
+			//turret_Gunmount.transform.Rotate(0, deltaRotation.x ,0);
+			Debug.DrawRay(turret_GunPoint.transform.position, targetVector.normalized, Color.green, 1);
+
+			Debug.Log("delta rot: "+deltaRotation);
+
 		}
 	}
 
