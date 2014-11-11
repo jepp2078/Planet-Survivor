e6e91c6844a655d5528187f342648f6835be0997
bubbles100 <bubbles100j@gmail.com>
2014-11-11 16:11:24 +0100

    Turret working!!

---
+++ Assets/bulletScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletScript : MonoBehaviour {
+	private float starttime;
+	// Use this for initialization
+	void Start () {
+		starttime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time - starttime > 1) {
+			Destroy (this.gameObject);
+		}
+	}
+	
+	void OnCollisionEnter(Collision collision) {
+		foreach (ContactPoint contact in collision.contacts) {
+			Debug.DrawRay(contact.point, contact.normal, Color.white);
+		}
+		if (collision.gameObject.name == "Target") {
+
+		}
+	}
+}
--- Assets/turret_script.cs
+++ Assets/turret_script.cs
@@ -3,30 +3,57 @@ using System.Collections;
 
 public class turret_script : MonoBehaviour {
 	public GameObject turret_Body, turret_Gunmount, turret_Gun1, turret_Gun2, turret_GunPoint;
+	public float RotationSpeed;
+	public float shotDelay;
+	public Rigidbody projectile; 
+	public float gunpower;
 
 	private Quaternion startYaw, startPitch, start_turret_GunPoint; 
+	private Quaternion _lookRotation;
+	private Vector3 _direction;
+
+	private float lastShotTime;
 
 	// Use this for initialization
 	void Start () {
 		startYaw = turret_Body.transform.rotation;
 		startPitch = turret_Gunmount.transform.rotation;
 		start_turret_GunPoint = turret_GunPoint.transform.rotation;
+
+		lastShotTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		GameObject Enemy = FindClosestEnemy();
 		if(Enemy != null){
-			Debug.DrawRay(turret_GunPoint.transform.position, turret_GunPoint.transform.forward, Color.red, 0.1f);
-			Vector3 targetVector = Enemy.transform.position - turret_GunPoint.transform.position;
-			Vector3 deltaRotation = turret_GunPoint.transform.forward - targetVector.normalized;
-			turret_Body.transform.Rotate(0, deltaRotation.x-deltaRotation.z, 0);
-			//turret_Gunmount.transform.Rotate(0, deltaRotation.x ,0);
-			Debug.DrawRay(turret_GunPoint.transform.position, targetVector.normalized, Color.green, 1);
 
-			Debug.Log("delta rot: "+deltaRotation);
+			_direction = (Enemy.transform.position - transform.position).normalized;
+			_direction.y = 0;
+			_lookRotation = Quaternion.LookRotation(_direction);
+			Quaternion slerpRot = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
+			transform.rotation = slerpRot;
 
+			_direction = (Enemy.transform.position - turret_Gunmount.transform.position).normalized;
+			_lookRotation = Quaternion.LookRotation(_direction);
+
+			_lookRotation *= Quaternion.Euler(90, -90, 0);
+
+			slerpRot = Quaternion.Slerp(turret_Gunmount.transform.rotation, _lookRotation, Time.deltaTime * RotationSpeed);
+			turret_Gunmount.transform.rotation = slerpRot;
 		}
+
+		if(Time.time - lastShotTime > shotDelay){
+			Debug.Log("bang!");
+			lastShotTime = Time.time;
+			shoot (Enemy);
+		}
+	}
+
+	void shoot(GameObject target){
+		Rigidbody tempProjectile;
+		tempProjectile = Instantiate(projectile, turret_GunPoint.transform.position, turret_GunPoint.transform.rotation) as Rigidbody;
+		tempProjectile.velocity = turret_GunPoint.transform.TransformDirection(Vector3.forward * gunpower);
 	}
 
 	GameObject FindClosestEnemy() {
